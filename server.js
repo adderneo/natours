5991c74e8c63ee4fe4846846b8933b7942afe32e
Adder <adder.neo@gmail.com>
2020-10-14 22:03:12 +0530

    Server confilg file

--- server.js
+++ server.js
@@ -55,3 +55,10 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
   });
 });
+
+process.on('SIGTERM', () => {
+  console.log('👽 🖖SIGTERM RECEIVED, Shutting down gracefully.');
+  server.close(() => {
+    console.log('🎇 🧨 Process terminated.');
+  });
+});
