785b8c6445e6fa059dfdddd6feca653b03285188
Adder <adder.neo@gmail.com>
2020-10-16 11:09:57 +0530

    Stripe bug

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -62,7 +62,7 @@ exports.getCheckoutSession = catchAsync(async (req, res, next) => {
 const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = await User.findOne({ email: session.customer_email }).id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.display_items[0].amount / 100;
   await Booking.create({ tour, user, price });
 };
 
@@ -79,7 +79,7 @@ exports.webHookCheckout = catchAsync(async (req, res, next) => {
     return res.status(400).send(`Webhook Eroor: ${err.message}`);
   }
 
-  if (event.type === 'checkout.session.complete')
+  if (event.type === 'checkout.session.completed')
     createBookingCheckout(event.data.object);
 
   res.status(200).json({ received: true });
